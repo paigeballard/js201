70c83b6868f30c872e28debf53f454f7e9860cc0
Paige Ballard <p.ballardhou@gmail.com>
2019-06-27 18:37:16 -0500

    finished the first 2 js 201 - DC tutorial 2

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -2,9 +2,22 @@
 // string that says hello to the name.
 // Example: hello('Mustache') should return 'Hello, Mustache!'
 
-
+function hello(name) {
+    let greeting = "Hello, ";
+    return greeting + name + "!";
+}
+hello("Mustache");
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+function helloDefault(name) {
+    let greeting = "Hello, ";
+
+    if (name === "") {
+        return "Hello, world!"
+    } else {
+        return greeting + name + "!";
+    }
+}
